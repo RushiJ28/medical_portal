536fc97400ed1fc4ab2ab1b86dafe4ec2f88d433
Rushikesh Jadhao <rushikesh@rapidken.ai.com>
2025-02-17 17:38:11 +0530

    created search patient page

---
+++ frontend/src/pages/SearchPatient/SearchPatient.jsx
@@ -0,0 +1,21 @@
+import "./SearchPatient.css";
+import { useState } from "react";
+import CustomForm from "../../components/CustomForm/CustomForm";
+
+const SearchPatient = () => {
+  const [idnumber, setIdNumber] = useState("");
+
+  return (
+    <div className="searchpatient-container">
+      <h2>Search</h2>
+      <CustomForm>
+        <CustomForm.IDNumber
+          value={idnumber}
+          onChange={(e) => setIdNumber(e.target.value)}
+        />
+      </CustomForm>
+    </div>
+  );
+};
+
+export default SearchPatient;
