347baeb8034a5c9326af591d5cf6989f5c2847e8
Rushikesh Jadhao <rushikesh@rapidken.ai.com>
2025-02-18 17:56:52 +0530

    added signin option in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -16,6 +16,9 @@ const Header = () => {
           <li className="nav-item">
             <Link to="/searchpatient">Search</Link>
           </li>
+          <li className="nav-item">
+            <Link to="/signin">Signin</Link>
+          </li>
         </ul>
       </nav>
     </header>
