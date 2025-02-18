1b4e3582e30e308508dcd3218bd85ddc588353af
Rushikesh Jadhao <rushikesh@rapidken.ai.com>
2025-02-18 14:30:55 +0530

    created router links

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import About from "./pages/About/About.jsx";
+import SearchPatient from "./pages/SearchPatient/SearchPatient.jsx";
+import Signin from "./pages/Signin/Signin.jsx";
+import RegisterPatient from "./pages/RegisterPatient/RegisterPatient.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegisterDoctor from "./pages/RegisterDoctor/RegisterDoctor.jsx";
+import SearchDoctor from "./pages/SearchDoctor/SearchDoctor.jsx";
+import Error from "./components/Error/Error.jsx";
+import Layout from "./Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/about", element: <About /> },
+      { path: "/searchpatient", element: <SearchPatient /> },
+      { path: "/signin", element: <Signin /> },
+      { path: "/registerpatient", element: <RegisterPatient /> },
+      { path: "/registerdoctor", element: <RegisterDoctor /> },
+      { path: "/searchdoctor", element: <SearchDoctor /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <RouterProvider router={router} />
+  // </StrictMode>,
+);
