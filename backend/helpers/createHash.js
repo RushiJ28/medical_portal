a2b3f3c23e765135c5d5233b051470e9a8c55fbf
Rushikesh Jadhao <rushikesh@rapidken.ai.com>
2025-02-18 15:04:08 +0530

    added a hash function to store password as hash value

---
+++ backend/helpers/createHash.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcrypt"); //library used to store hash password in DB and not the plain password text.
+
+const sailtRounds = 10;
+
+async function createHash(password) {
+  return bcrypt
+    .hash(password, sailtRounds)
+    .then((hash) => {
+      console.log(hash);
+      return hash;
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+}
+
+module.export = createHash;
