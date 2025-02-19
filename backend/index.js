3acd869727fc6a7f312e18f5f390c667208aafcf
Rushikesh Jadhao <rushikesh@rapidken.ai.com>
2025-02-19 15:16:49 +0530

    added routes for different pages and added middleware to handle them

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
 require("dotenv").config();
+const express = require("express");
+//It allows us to comunicate betw backend and frontend it cd be hhtp/https
+const cors = require("cors");
+
+const patientsRouter = require("./routes/patients");
+const doctorsRouter = require("./routes/doctors");
+const usersRouter = require("./routes/users");
+const returnStatus = require("./helpers/returnStatus");
+//Create an instance of Express
+const app = express();
+//Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors());
+
+//apply route middleware to these routes
+app.use("/patients", patientsRouter);
+app.use("/doctors", doctorsRouter);
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello...");
+});
+
+//Error handling middleware for non-exisiting routes
+app.use((req, res, next) => {
+  return returnStatus(res, 404, true, "Not Found");
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server is running on port: ", port);
+});
