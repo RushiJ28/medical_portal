02da049775f6b435c057c61a55de243fd52d4385
Rushikesh Jadhao <rushikesh@rapidken.ai.com>
2025-02-19 15:17:30 +0530

    added port and corrected token function to return

--- backend/helpers/signToken.js
+++ backend/helpers/signToken.js
@@ -2,6 +2,7 @@ var jwt = require("jsonwebtoken");
 
 function signToken(payload) {
   const token = jwt.sign(payload, process.env.secret, { expiresIn: "1h" });
+  return token;
 }
 
 module.exports = signToken;
